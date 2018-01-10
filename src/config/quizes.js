6b5e0582dfd106d21fa4bfedf8ff721178204b55
KrasiStoyanov <krasimir641@gmail.com>
2018-01-10 18:36:28 +0100

    Added all quiz data

--- src/config/quizes.js
+++ src/config/quizes.js
@@ -64,7 +64,7 @@ export default {
 				},
 				{
 					"id": 5,
-					"question": "All Shaw Fall",
+					"question": "All Shall Fall",
 					"correctAnswer": "Immortal",
 					"answers": [
 						"Nigtwish",
@@ -128,112 +128,112 @@ export default {
 			"questions": [
 				{
 					"id": 0,
-					"question": "Breaking the Law",
-					"correctAnswer": "Judas Priest",
+					"question": "Insomnium",
+					"correctAnswer": "While We Sleep",
 					"answers": [
-						"Metallica",
-						"Iron Maiden",
-						"Insomnium",
-						"Judas Priest"
+						"Enter Sandman",
+						"While We Sleep",
+						"Nymphetamine Fix",
+						"Hallowed Be Thy Name"
 					]
 				},
 				{
 					"id": 1,
-					"question": "Master Passion Greed",
-					"correctAnswer": "Nigtwish",
+					"question": "Cannibal Corpse",
+					"correctAnswer": "Hammer Smashed Face",
 					"answers": [
-						"Slipknot",
-						"Death",
-						"Nigtwish",
-						"Sepultura"
+						"Almost Easy",
+						"Hail to the King",
+						"Your Betrayal",
+						"Hammer Smashed Face"
 					]
 				},
 				{
 					"id": 2,
-					"question": "Roots Bloody Roots",
-					"correctAnswer": "Sepultura",
+					"question": "Babymetal",
+					"correctAnswer": "line!",
 					"answers": [
-						"Sepultura",
-						"Dark Tranquility",
-						"System of a Down",
-						"Children of Bodom"
+						"line!",
+						"What's Up, People",
+						"Bu-ikikaesu!!",
+						"Kyōkatsu"
 					]
 				},
 				{
 					"id": 3,
-					"question": "The Satanist",
-					"correctAnswer": "Behemoth",
+					"question": "Black Sabbath",
+					"correctAnswer": "Children of the Grave",
 					"answers": [
-						"Behemoth",
-						"Sepultura",
-						"Metallica",
-						"Death"
+						"Until Eternity",
+						"Children of the Grave",
+						"Twilight of the God",
+						"Reborn"
 					]
 				},
 				{
 					"id": 4,
-					"question": "I Won't See You Tonight (Part 2)",
-					"correctAnswer": "Avenged Sevenfold",
+					"question": "Stone Sour",
+					"correctAnswer": "Fabuless",
 					"answers": [
-						"Bring Me the Horizon",
-						"Immortal",
-						"Gwar",
-						"Avenged Sevenfold"
+						"Eagle Eyes",
+						"Fabuless",
+						"Demons in You",
+						"Control"
 					]
 				},
 				{
 					"id": 5,
-					"question": "All Shaw Fall",
-					"correctAnswer": "Immortal",
+					"question": "Six Feet Under",
+					"correctAnswer": "Sacrificial Kill",
 					"answers": [
-						"Nigtwish",
-						"Slipknot",
-						"Immortal",
-						"Twisted Sister"
+						"Sacrificial Kill",
+						"Believe",
+						"The Last in Line",
+						"Holly Diver"
 					]
 				},
 				{
 					"id": 6,
-					"question": "Contractor",
-					"correctAnswer": "Lamb of God",
+					"question": "Lamb of God",
+					"correctAnswer": "The Black Dahila",
 					"answers": [
-						"Megadeth",
-						"Slipknot",
-						"Lamb of God",
-						"Trivium"
+						"Alive",
+						"The Black Dahila",
+						"Did My Time",
+						"Liar"
 					]
 				},
 				{
 					"id": 7,
-					"question": "We're Not Gonna Take It",
-					"correctAnswer": "Twisted Sister",
+					"question": "Kamelot",
+					"correctAnswer": "Liar Liar (Wasteland Monarchy)",
 					"answers": [
-						"Twisted Sister",
-						"Nigtwish",
-						"Death",
-						"Iron Maiden"
+						"Symphony of Destruction",
+						"I Speak Astronomy",
+						"Liar Liar (Wasteland Monarchy)",
+						"Angel"
 					]
 				},
 				{
 					"id": 8,
-					"question": "Symphony of Destruction",
-					"correctAnswer": "Megadeth",
+					"question": "Human Error",
+					"correctAnswer": "Synthetic Euphoria",
 					"answers": [
-						"Megadeth",
-						"Metallica",
-						"AC/DC",
-						"Lamb of God"
+						"I Want Out",
+						"Arise",
+						"Hang 'Em High",
+						"Synthetic Euphoria"
 					]
 				},
 				{
 					"id": 9,
-					"question": "In the Name of God",
-					"correctAnswer": "Dream Theater",
+					"question": "Havok",
+					"correctAnswer": "Arise",
 					"answers": [
-						"Heaven Shall Burn",
-						"Babymetal",
-						"Dream Theater",
-						"Born of Osiris"
+						"Hacksaw Surgery",
+						"Arise",
+						"Siren Halo",
+						"Sweet Child O' Mine"
 					]
 				}
 			]
@@ -243,21 +243,357 @@ export default {
 			"title": "Genre based on band",
 			"subtitle": "Guess the genre of each one of the bands",
 			"coverImage": "./images/slide-3.jpg",
-			"link": "./quiz-3"
+			"link": "./quiz-3",
+			"questions": [
+				{
+					"id": 0,
+					"question": "Korn",
+					"correctAnswer": "Nu Metal",
+					"answers": [
+						"Heavy Metal",
+						"Death Metal",
+						"Nu Metal",
+						"Grindcore"
+					]
+				},
+				{
+					"id": 1,
+					"question": "Burzum",
+					"correctAnswer": "Black Metal",
+					"answers": [
+						"Hard Rock",
+						"Black Metal",
+						"Extreme Metal",
+						"Funk"
+					]
+				},
+				{
+					"id": 2,
+					"question": "Amon Amarth",
+					"correctAnswer": "Viking Metal",
+					"answers": [
+						"Nu Metal",
+						"Christian Metal",
+						"Unblack Metal",
+						"Viking Metal"
+					]
+				},
+				{
+					"id": 3,
+					"question": "Six Feet Under",
+					"correctAnswer": "Death Metal",
+					"answers": [
+						"Death Metal",
+						"Technical Death Metal",
+						"Brutal Death Metal",
+						"Thrash Metal"
+					]
+				},
+				{
+					"id": 4,
+					"question": "Metallica",
+					"correctAnswer": "Thrash Metal",
+					"answers": [
+						"Rock & Roll",
+						"Thrash Metal",
+						"Heavy Metal",
+						"Punk"
+					]
+				},
+				{
+					"id": 5,
+					"question": "Megadeth",
+					"correctAnswer": "Thrash Metal",
+					"answers": [
+						"Thrash Metal",
+						"Death Metal",
+						"Doom Metal",
+						"Nu Metal"
+					]
+				},
+				{
+					"id": 6,
+					"question": "Ghost",
+					"correctAnswer": "Doom Metal",
+					"answers": [
+						"Doom Metal",
+						"Black Metal",
+						"Gothic Metal",
+						"Nu Metal"
+					]
+				},
+				{
+					"id": 7,
+					"question": "Nightwish",
+					"correctAnswer": "Symphonic Metal",
+					"answers": [
+						"Rock & Roll",
+						"Metal",
+						"Symphonic Metal",
+						"Funeral Doom"
+					]
+				},
+				{
+					"id": 8,
+					"question": "Gojira",
+					"correctAnswer": "Technical Death Metal",
+					"answers": [
+						"Metal",
+						"Technical Death Metal",
+						"Power Metal",
+						"Heavy Metal"
+					]
+				},
+				{
+					"id": 9,
+					"question": "Manowar",
+					"correctAnswer": "Power Metal",
+					"answers": [
+						"Thrash Metal",
+						"Power Metal",
+						"Rock & Roll",
+						"Metal"
+					]
+				}
+			]
 		},
 		{
 			"id": 3,
 			"title": "Album based on band",
 			"subtitle": "Choose the correct album for each band",
 			"coverImage": "./images/slide-4.jpg",
-			"link": "./quiz-4"
+			"link": "./quiz-4",
+			"questions": [
+				{
+					"id": 0,
+					"question": "Oceano",
+					"correctAnswer": "Ascendants",
+					"answers": [
+						"Ascendants",
+						"Hardwired...To Self-Destruct",
+						"Ride The Lightning",
+						"Rust in Peace"
+					]
+				},
+				{
+					"id": 1,
+					"question": "Megadeth",
+					"correctAnswer": "Peace Sells...But Who's Buying",
+					"answers": [
+						"Flesh Coffin",
+						"Get Heavy",
+						"Holy Wood",
+						"Peace Sells...But Who's Buying"
+					]
+				},
+				{
+					"id": 2,
+					"question": "Limp Bizkit",
+					"correctAnswer": "Significant Other",
+					"answers": [
+						"Significant Other",
+						"Revenge",
+						"Korn",
+						"Follow the Leader"
+					]
+				},
+				{
+					"id": 3,
+					"question": "Korn",
+					"correctAnswer": "The Serenity of Suffering",
+					"answers": [
+						"Killswitch Engage",
+						"Heaven's Venom",
+						"The Serenity of Suffering",
+						"Nostradamus"
+					]
+				},
+				{
+					"id": 4,
+					"question": "Judas Priest",
+					"correctAnswer": "Angel of Retribution",
+					"answers": [
+						"Angel of Retribution",
+						"The Landing",
+						"Asylum",
+						"Mold"
+					]
+				},
+				{
+					"id": 5,
+					"question": "Disturbed",
+					"correctAnswer": "Immortalized",
+					"answers": [
+						"Immortalized",
+						"Powerslave",
+						"Battles",
+						"Dystopia"
+					]
+				},
+				{
+					"id": 6,
+					"question": "Guttural Slug",
+					"correctAnswer": "Megalodon",
+					"answers": [
+						"Got Your Six",
+						"War is the Answer",
+						"Megalodon",
+						"American Capitalist"
+					]
+				},
+				{
+					"id": 7,
+					"question": "Fleshgod Apocalypse",
+					"correctAnswer": "King",
+					"answers": [
+						"Deathgrip",
+						"Dreamless",
+						"King",
+						"Demanufacture"
+					]
+				},
+				{
+					"id": 8,
+					"question": "Fear Factory",
+					"correctAnswer": "Archetype",
+					"answers": [
+						"Blood In Blood Out",
+						"Drift",
+						"Axioma Ethica Odini",
+						"Archetype"
+					]
+				},
+				{
+					"id": 9,
+					"question": "Rings of Saturn",
+					"correctAnswer": "Lugal Ki En",
+					"answers": [
+						"Lugal Ki En",
+						"The Endgame",
+						"Thornography",
+						"Great is Our Sin"
+					]
+				}
+			]
 		},
 		{
 			"id": 4,
 			"title": "Vocalist based on band",
 			"subtitle": "Guess the lead singer of each band",
 			"coverImage": "./images/slide-5.jpg",
-			"link": "./quiz-5"
+			"link": "./quiz-5",
+			"questions": [
+				{
+					"id": 0,
+					"question": "Metallica",
+					"correctAnswer": "James Hetfield",
+					"answers": [
+						"Lars Ulrich",
+						"Dave Mustaine",
+						"James Hetfield",
+						"Alex Shikolai"
+					]
+				},
+				{
+					"id": 1,
+					"question": "Megadeth",
+					"correctAnswer": "Dave Mustaine",
+					"answers": [
+						"Ronnie James Dio",
+						"David Ellefson",
+						"James Hetfield",
+						"Dave Mustaine"
+					]
+				},
+				{
+					"id": 2,
+					"question": "Black Sabbath",
+					"correctAnswer": "Ozzy Osbourne",
+					"answers": [
+						"Ozzy Osbourne",
+						"David Keith",
+						"James Hetfield",
+						"Ben Duerr"
+					]
+				},
+				{
+					"id": 3,
+					"question": "Shadow of Intent",
+					"correctAnswer": "Ben Duerr",
+					"answers": [
+						"Lars Ulrich",
+						"Kirk Hammet",
+						"Ben Duerr",
+						"Alex Shikolai"
+					]
+				},
+				{
+					"id": 4,
+					"question": "Slaughter to Prevail",
+					"correctAnswer": "Alex Shikolai",
+					"answers": [
+						"Mike Petrov",
+						"Jared Delgado",
+						"Alex Shikolai",
+						"Denislav Nikolov"
+					]
+				},
+				{
+					"id": 5,
+					"question": "Rejuvenation",
+					"correctAnswer": "Denislav Nikolov",
+					"answers": [
+						"Toni Ivanov",
+						"Denislav Nikolov",
+						"Yoan Moskov",
+						"Serj Tankian"
+					]
+				},
+				{
+					"id": 6,
+					"question": "System of a Down",
+					"correctAnswer": "Serj Tankian",
+					"answers": [
+						"Serj Tankian",
+						"Tarja Turunen",
+						"Nergal",
+						"Freddie Mercury"
+					]
+				},
+				{
+					"id": 7,
+					"question": "Disturbed",
+					"correctAnswer": "David Draiman",
+					"answers": [
+						"David Draiman",
+						"Frank Mullen",
+						"Randy Blythe",
+						"Barney Greenway"
+					]
+				},
+				{
+					"id": 8,
+					"question": "Disturbed",
+					"correctAnswer": "David Draiman",
+					"answers": [
+						"Klaus Meine",
+						"David Draiman",
+						"David Coverdale",
+						"Chuck Schuldiner"
+					]
+				},
+				{
+					"id": 9,
+					"question": "Cradle of Filth",
+					"correctAnswer": "Daniel Davey",
+					"answers": [
+						"Serj Tankian",
+						"Jonathan Davis",
+						"Daniel Davey",
+						"Al Jourgensen"
+					]
+				}
+			]
 		}
 	]
 }
